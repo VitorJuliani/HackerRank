57824d2d03c56a51bf03b5486f970afcf1d739ea
Vitor Juliani <vitorjuliani99@gmail.com>
2020-05-24 03:10:54 -0300

    Solve me first problem

---
+++ src/SolveMeFirst.kt
@@ -0,0 +1,13 @@
+import java.util.*
+
+fun solveMeFirst(num1: Int, num2: Int): Int {
+    return num1 + num2
+}
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val num1 = sc.nextInt()
+    val num2 = sc.nextInt()
+    val sum = solveMeFirst(num1, num2)
+    println(sum)
+}
